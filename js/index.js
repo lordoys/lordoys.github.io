513db63482b579d16ee1e9ce9fbdc238fafedb3b
pecherin.m <lordoys@gmail.com>
2022-12-22 19:43:33 +0200

    One more

--- js/index.js
+++
@@ -1,8 +0,0 @@
-import Api from "./Api.js";
-import CardList from "./CardList.js";
-
-let newApi = new Api();
-let response = await newApi.getData();
-let cardList = new CardList(response);
-
-cardList.createList();
